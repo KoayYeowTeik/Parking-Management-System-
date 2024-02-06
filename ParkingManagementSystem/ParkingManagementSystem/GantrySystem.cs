b004badf9cb0f3c1ca7ffaa7e2ead95655840254
Hadith Mukrish Mohammad Hamidi <s10221013@connect.np.edu.sg>
2024-02-06 23:27:07 +0800

    updated gantry class

--- ParkingManagementSystem/ParkingManagementSystem/GantrySystem.cs
+++ ParkingManagementSystem/ParkingManagementSystem/GantrySystem.cs
@@ -10,14 +10,41 @@ namespace ParkingManagementSystem
     {
         public List<ParkingRecord> parkRecords { get; set; }
         public GantrySystem() { }   
+        
+        public string VehicleType { get; set; }
+
+        public DateTime EntryTime { get; set; }
+
+        public DateTime ExitTime { get; set; }
+
+        public double Rate { get; set; }
+
+        public string Pass { get; set; }
+
         public GantrySystem(List<ParkingRecord> parkRecords)
         {
             this.parkRecords = parkRecords;
         }
-        public void AddRecord() { }
-        public decimal CalculateCharge()
+        public void AddRecord() 
+        { 
+            
+        
+        }
+        public decimal CalculateCharge(string vehicleType, DateTime EntryTime, DateTime ExitTime, Double rate, string pass)
         {
-            return 0;
+            decimal totalCharge = 0;
+            VehicleType = VehicleType;
+            this.EntryTime = EntryTime;
+            this.ExitTime = ExitTime;
+            Rate = rate;
+            Pass = pass;
+
+            bool isValid = CheckPass(pass);
+
+            //get the parking pass if it exists, if daily pass calculate based on maximum rate, otherwise charge normally.
+            
+            //implementation
+            return totalCharge;
         }
         public decimal CalculateRevenue()
         {
@@ -27,8 +54,9 @@ namespace ParkingManagementSystem
         {
             
         }
-        public bool CheckPass()
+        public bool CheckPass(string)
         {
+            //implementation
             return false;
         }
         public void RemoveRecord(int id)
